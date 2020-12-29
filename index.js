69b8a5f6272b01c6391d4c372a0528ef496ff1d7
Rayhaan Khalid <rkhalid@me.com>
2020-12-29 17:49:19 +0000

    change endpoint, refactor

--- index.js
+++ index.js
@@ -4,45 +4,43 @@ const app = express()
 const scraper = require('./scraper')
 const converter = require('./utils/24hrConverter')
 
-
-app.get('/', async (req, res) => {
-
-const [times, jTimes] = await scraper()
-
-const [fajr, sunrise, ...othertimes] = times
-
-const convertedTimes = othertimes.map(time => converter(time))
-
-const [dhuhr, asr, maghrib, isha] = convertedTimes
-
-const prayerTimes = {
-    fajr,
-    sunrise,
-    dhuhr,
-    asr,
-    maghrib,
-    isha
-}
-
-const [fajrJamaa, ...otherJTimes] = jTimes
-
-const convertedJTimes = otherJTimes.map(time => converter(time))
-
-const [dhuhrJamaa, asrJamaa, maghribJamaa, ishaJamaa] = convertedJTimes
-
-const jamaaTimes = {
-    fajrJamaa,
-    dhuhrJamaa,
-    asrJamaa,
-    maghribJamaa,
-    ishaJamaa
-}
-
-res.json({prayerTimes, jamaaTimes})
+const PORT = process.env.PORT || 3000
 
 
+app.get('/getPrayerTimes', async (req, res) => {
+
+    //scrape times
+
+    const [times, jTimes] = await scraper()
+
+    const [fajr, sunrise, ...othertimes] = times
+    const [fajrJamaa, ...otherJTimes] = jTimes
+    //convert times to 24hrs
+    const convertedTimes = othertimes.map(time => converter(time))
+    const convertedJTimes = otherJTimes.map(time => converter(time))
+
+    const [dhuhr, asr, maghrib, isha] = convertedTimes
+    const [dhuhrJamaa, asrJamaa, maghribJamaa, ishaJamaa] = convertedJTimes
+
+    const prayerTimes = {
+        fajr,
+        sunrise,
+        dhuhr,
+        asr,
+        maghrib,
+        isha
+    }
+    const jamaaTimes = {
+        fajrJamaa,
+        dhuhrJamaa,
+        asrJamaa,
+        maghribJamaa,
+        ishaJamaa
+    }
+
+    res.json({prayerTimes, jamaaTimes})
 
 })
 
-app.listen(3000, () => console.log('server started'))
+app.listen(PORT, () => console.log(`server stared on port ${PORT}`))
 
